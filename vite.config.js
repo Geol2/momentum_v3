0353134a43cc15967990c1ecd10835d9366ea051
Geol2 <big9401@gmail.com>
2026-07-16 17:26:22 +0900

    add : 사이트 맵!

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
-     sitemap({
-      hostname: 'https://momentum.geol2.com'
-    })
+      sitemap({
+        hostname: 'https://momentum.geol2.com'
+      })
   ],
   server: {
     proxy: {
